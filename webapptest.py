586ea666ff07cf5735315ba2a85da440269d517a
Florence Coniquet <fluette@gmail.com>
2016-11-19 16:21:49 -0600

    Forms and Input HW

---
+++ webapptest.py
@@ -0,0 +1,20 @@
+import webapp2
+
+form="""
+<form method="post">
+    What is your birthday?
+    <br>
+    <label>Month <input type="text" name"month"></label>
+    <label>Day <input type="text" name"day"></label>
+    <label>Year <input type="text" name"year"></label>
+    <br>
+    <br>
+    <input type="submit">
+</form>
+"""
+
+class MainPage(webapp2.RequestHandler):
+	def get(self):
+		self.response.out.write(form)
+
+app = webapp2.WSGIApplication([('/', MainPage)], debug=True)
